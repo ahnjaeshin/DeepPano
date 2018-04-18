90cfd35e1e4d53f0fc47754fa4bf7568c721fc6a
yuyupopo <yuyupopo@snu.ac.kr>
2018-04-18 21:44:48 +0900

    better logging, load/store

--- src/trainer.py
+++ src/trainer.py
@@ -21,18 +21,19 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-import torchvision.utils as utils
+from torchvision.utils import make_grid
 from torchvision import datasets
 from torch.autograd import Variable
 
-from utils import slack_message, count_parameters
+from utils import slack_message, count_parameters, AverageMeter
 from torch.nn import functional as F
-import tabulate
+import shutil
 
 
-def cuda(x):
-    """for use in gpu"""
-    return x.cuda(async=True) if torch.cuda.is_available() else x
+def cuda(x, async=True):
+    """for use in gpu
+    add async=True"""
+    return x.cuda() if torch.cuda.is_available() else x
 
 class Trainer(): 
     """trainer class
@@ -49,7 +50,7 @@ class Trainer():
         ensemble!!
     """
 
-    def __init__(self, model, datasets, criterion, optimizer, scheduler, metrics):
+    def __init__(self, model, datasets, criterion, optimizer, scheduler, metrics, checkpoint=None):
         
         self.model = model
         self.datasets = datasets
@@ -57,20 +58,30 @@ class Trainer():
         self.optimizer = optimizer
         self.scheduler = scheduler
         self.metrics = metrics
+        self.checkpoint = checkpoint
+
+        self.start_epoch = 0
+        self.best_score = 0
+
+        if checkpoint:
+            self.load(checkpoint)
 
         if not torch.cuda.is_available(): 
             print("CUDA is unavailable. It'll be very slow...")
         
-        self.writer = SummaryWriter(comment="UNet")
+        self.trainWriter = SummaryWriter(comment="-train")
+        self.valWriter = SummaryWriter(comment="-val")
         dummy_input = Variable(torch.rand(1, 2, 224, 224), requires_grad=True)
-        self.writer.add_graph(self.model, (dummy_input, ))
-        self.writer.add_scalar('number of parameter', count_parameters(self.model))
-        self.writer.add_text('info', 'this is start', 0)
+
+        for writer in [self.trainWriter, self.valWriter]:
+            writer.add_graph(self.model, (dummy_input, ))
+            writer.add_scalar('number of parameter', count_parameters(self.model))
+            writer.add_text('info', 'this is start', 0)
 
     def train(self, batch_size = 16, num_workers = 32, epochs = 100, log_freq_ratio = 10):
         
         slack_message('train started', '#botlog')
-        slack_message(self.model.__repr__, '#botlog')
+        # slack_message(self.model.__repr__, '#botlog')
 
         dataloaders = { x: DataLoader(dataset = self.datasets[x], 
                                       batch_size = batch_size, 
@@ -86,8 +97,9 @@ class Trainer():
             self.model = torch.nn.DataParallel(self.model, device_ids=range(torch.cuda.device_count()))
 
         start_time = datetime.datetime.now()
+        self.best_score = 0
 
-        for epoch in tqdm(range(epochs), desc='epoch'):
+        for epoch in tqdm(range(self.start_epoch, epochs), desc='epoch'):
             self.train_once(epoch, dataloaders['train'], True, log_freq_ratio)
             self.train_once(epoch, dataloaders['val'], False, log_freq_ratio)
             
@@ -102,86 +114,94 @@ class Trainer():
 
     def train_once(self, epoch, dataloader, train=True, log_freq_ratio = 10):
         
-        batch_losses = []
-        forward_times = []
-        backward_times = []
-        data_times = []
-        curr_score = 0
+        losses = AverageMeter()
+        forward_times = AverageMeter()
+        backward_times = AverageMeter()
+        data_times = AverageMeter()
+        curr_scores = AverageMeter()
 
-        if train:
-            self.model.train(True)
-        else:
-            self.model.train(False) # same as self.model.eval()
+        best_score = 0
+
+        self.model.train(train)
+        state = 'train' if train else 'val'
+        writer = self.trainWriter if train else self.valWriter
 
         start = time.time()
-        for batch_idx, (data, target, filepath, index) in enumerate(tqdm(dataloader, desc='batch')):
-
-            data_time = time.time() - start
-            data_times.append(data_time)
-
-            data = cuda(Variable(data))
-            target = cuda(Variable(target))
+        for batch_idx, (input, target, filepath, index) in enumerate(tqdm(dataloader, desc='batch')):
             
-            output = self.model(data)
+            niter = epoch*len(dataloader) + batch_idx
+            data_times.update(time.time() - start)
+
+            input = cuda(Variable(input))
+            target = cuda(Variable(target))
+            output = self.model(input)
             loss = self.criterion(output, target)
             output = F.sigmoid(output)
-            
-            forward_time = time.time() - data_time
-            forward_times.append(forward_time)
+
+            losses.update(loss.cpu().data[0], input.cpu().size(0))
+            forward_times.update(time.time() - data_times.val)
 
             if train : 
                 self.optimizer.zero_grad()            
                 loss.backward()
                 self.optimizer.step()
                 
-            batch_loss = loss.cpu().data[0]
-            batch_losses.append(batch_loss)
-            backward_time = time.time() - forward_time
-            backward_times.append(backward_time)
+            backward_times.update(time.time() - forward_times.val)
             start = time.time()
 
-            batch_size = data.size(0)
-            niter = epoch*len(dataloader) + batch_idx
-
             for metric in self.metrics:
-                curr_score = metric.add(output.data.cpu().numpy(), target.data.cpu().numpy())
+                curr_score = 0
+                curr_score = curr_score + metric.add(output.data.cpu().numpy(), target.data.cpu().numpy())
+                curr_scores.update(curr_score)
 
             if batch_idx % log_freq_ratio == 0:
                 log = [
+                    '[{}]'.format(state),
                     'Epoch: [{0}][{1}/{2}]'.format(epoch, batch_idx, len(dataloader)),
-                    'Forward Time {batch_time:.3f} ({batch_time_avg:.3f})'.format(batch_time=forward_time, batch_time_avg=np.mean(forward_times)),
-                    'Backward Time {batch_time:.3f} ({batch_time_avg:.3f})'.format(batch_time=backward_time, batch_time_avg=np.mean(backward_times)),
-                    'Data {data_time:.3f} ({data_time_avg:.3f})'.format(data_time=data_time, data_time_avg=np.mean(data_times)),
-                    'Loss {loss:.4f} ({loss_avg:.4f})'.format(loss=batch_loss, loss_avg=np.mean(batch_losses)),
+                    'Forward Time {time.val:.3f} ({time.avg:.3f})'.format(time=forward_times),
+                    'Backward Time {time.val:.3f} ({time.avg:.3f})'.format(time=backward_times),
+                    'Data {time.val:.3f} ({time.avg:.3f})'.format(time=data_times),
+                    'Loss {loss.val:.4f} ({loss.avg:.4f})'.format(loss=losses),
                 ]
 
-                self.writer.add_scalar('time(forward)', forward_time, niter)
-                self.writer.add_scalar('time(backward)', backward_time, niter)
-                self.writer.add_scalar('time(data)', data_time, niter)
-                self.writer.add_scalar('loss', batch_loss, niter)
+                writer.add_scalar('time/forward', forward_times.val, niter)
+                writer.add_scalar('time/backward', backward_times.val, niter)
+                writer.add_scalar('time/data', data_times.val, niter)
+                writer.add_scalar('loss', losses.val, niter)
 
                 if train:
                     lr = [group['lr'] for group in self.optimizer.param_groups][0]
                     tqdm.write('Learning rate: {}'.format(lr))
-                    self.writer.add_scalar('learning rate', lr, epoch)
+                    writer.add_scalar('learning rate', lr, epoch)
+
+                    for tag, value in self.model.named_parameters():
+                        tag = tag.replace('.', '/')
+                        writer.add_histogram('model/(train)' + tag, value.data.cpu().numpy(), niter, bins='doane')
+                        writer.add_histogram('model/(train)' + tag + '/grad', value.grad.data.cpu().numpy(), niter, bins='doane')
 
                 for metric in self.metrics:
-                    log.append('metric-{}: {:.5f} ({:.5f})'.format(metric.__repr__(), metric.getRecentScore(), metric.mean()))
-                    self.writer.add_scalar('metric-{}'.format(metric.__repr__()), metric.getRecentScore(), niter)
-                    self.writer.add_histogram('metric-{}'.format(metric.__repr__()), metric.hist(), niter, bins='auto')
+                    log.append('metric/{}: {:.5f} ({:.5f})'.format(metric.__repr__(), metric.getRecentScore(), metric.mean()))
+                    writer.add_scalar('metric/{}'.format(metric.__repr__()), metric.getRecentScore(), niter)
                 log = "\n".join(log)
 
-                self.writer.add_image('input-pano', utils.make_grid(data.data.cpu().narrow(1, 1, 1), normalize=True, scale_each=True), niter)
-                self.writer.add_image('input-guideline', utils.make_grid(data.data.cpu().narrow(1, 0, 1), normalize=True, scale_each=True), niter)
-                self.writer.add_image('target', utils.make_grid(target.data.cpu(), normalize=True, scale_each=True), niter)
-                self.writer.add_image('output', utils.make_grid(output.data.cpu(), normalize=True, scale_each=True), niter)
-                self.writer.add_pr_curve('accuracy', target.data.cpu(), output.data.cpu(), niter)
+                writer.add_image('input/pano', make_grid(input.data.cpu().narrow(1, 1, 1), normalize=True, scale_each=True), niter)
+                writer.add_image('input/guideline', make_grid(input.data.cpu().narrow(1, 0, 1), normalize=True, scale_each=True), niter)
+                writer.add_image('target', make_grid(target.data.cpu(), normalize=True, scale_each=True), niter)
+                writer.add_image('output', make_grid(output.data.cpu(), normalize=True, scale_each=True), niter)
+                
+                writer.add_pr_curve('accuracy', target.data.cpu(), output.data.cpu(), niter)
 
                 tqdm.write(log)
-                slack_message(log, '#botlog')
+                # slack_message(log, '#botlog')
+
+
+                is_best = self.best_score < curr_score
+                self.best_score = max(self.best_score, curr_score)
+
+                self.save_checkpoint(epoch, is_best)
                 
         if train: # smoothing effect to LR reduce on platue
-            self.scheduler.step(np.mean(batch_losses))
+            self.scheduler.step(losses.avg)
 
         # todo : for every batch, log hard examples
 
@@ -191,8 +211,40 @@ class Trainer():
     def ensemble(self):
         pass
 
-    def save(self):
-        pass
+    def save_checkpoint(self, epoch, is_best):
 
-    def load(self):
-        pass
+        if torch.cuda.device_count() > 1:
+            model = self.model.module
+        else:
+            model = self.model
+
+        filename = "../result/checkpoint-{}-{}.pth.tar".format(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M'), epoch)
+
+        state = {
+            'epoch': epoch + 1,
+            'state_dict': model.state_dict(),
+            'model': model.__class__.__name__,
+            'best_score': self.best_score,
+            'optimizer' : self.optimizer.state_dict(),
+        }
+        torch.save(state, filename)
+        if is_best:
+            shutil.copyfile(filename, "../result/checkpoint-best-{}-{}.pth.tar".format(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M'), epoch))
+
+    def load(self, path):
+        if os.path.isfile(path):
+            tqdm.write("=> loading checkpoint '{}'".format(path))
+            slack_message("=> loading checkpoint '{}'".format(path))
+
+            checkpoint = torch.load(path)
+            self.start_epoch = checkpoint['epoch']
+            self.best_score = checkpoint['best_score']
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+
+            tqdm.write("=> loaded checkpoint '{}' (epoch {})".format(path, checkpoint['epoch']))
+            slack_message("=> loaded checkpoint '{}' (epoch {})".format(path, checkpoint['epoch']))
+        else:
+            tqdm.write("=> no checkpoint found at '{}'".format(path))
+            slack_message("=> no checkpoint found at '{}'".format(path))
+
