fix metric (average by image, not batch)

src/metric.py
@@ -23,15 +23,26 @@ class IOU(Metric):
     def eval(self, *measure):
         output = measure[0]
         target = measure[1]
+        batch_size = output.shape[0]
+
+        assert output.shape[0] == target.shape[0]
+        output = np.reshape(output, (batch_size, -1))
+        target = np.reshape(target, (batch_size, -1))
+        assert output.shape[1] == target.shape[1]
         assert (output <= 1).all() and (output >= 0).all()
         assert (set(np.unique(target)) == {0,1} ) or (set(np.unique(target)) == {0}) or (set(np.unique(target)) == {1})
+    
         out = (output > self.threshold).astype(int)
-        union = np.logical_or(out, target).sum()
-        intersection = np.logical_and(out, target).sum()
+        union = np.logical_or(out, target).sum(axis=-1)
+        intersection = np.logical_and(out, target).sum(axis=-1)
 
-        if union == 0 and intersection == 0:
-            return 1
-        return intersection / union
+        assert union.shape[0] == batch_size and intersection.shape[0] == batch_size
+
+        zero_indices = np.where(union == 0)
+        union[zero_indices] = 1
+        intersection[zero_indices] = 1
+
+        return np.mean(np.divide(intersection, union))
 
     def __repr__(self):
         return 'IoU, threshold: {}'.format(self.threshold)
@@ -44,17 +55,50 @@ class DICE(Metric):
     def eval(self, *measure):
         output = measure[0]
         target = measure[1]
+        batch_size = output.shape[0]
+
+        assert output.shape[0] == target.shape[0]
+        output = np.reshape(output, (batch_size, -1))
+        target = np.reshape(target, (batch_size, -1))
+        assert output.shape[1] == target.shape[1]
         assert (output <= 1).all() and (output >= 0).all()
         assert (set(np.unique(target)) == {0,1} ) or (set(np.unique(target)) == {0}) or (set(np.unique(target)) == {1})
+    
         out = (output > self.threshold).astype(int)
-        union = np.logical_or(out, target).sum()
-        intersection = np.logical_and(out, target).sum()
+        union = np.logical_or(out, target).sum(axis=-1)
+        intersection = np.logical_and(out, target).sum(axis=-1)
 
-        if union == 0 and intersection == 0:
-            return 1
-        return (intersection * 2) / (union + intersection)
+        assert union.shape[0] == batch_size and intersection.shape[0] == batch_size
+
+        zero_indices = np.where(union == 0)
+        union[zero_indices] = 1
+        intersection[zero_indices] = 1
+
+        union = union + intersection
+        intersection = intersection * 2
+
+        return np.mean(np.divide(intersection, union))
 
     def __repr__(self):
         return 'DICE, threshold: {}'.format(self.threshold)
 
 
+class F1BySegment(Metric):
+    """[summary]
+    
+    Arguments:
+        Metric {[type]} -- [description]
+    
+    Returns:
+        [type] -- [description]
+    """
+
+    
+    def __init__(self, threshold = 0.5):
+        super(F1BySegment, self).__init__(threshold)
+
+    def eval(self, *measure):
+        raise NotImplementedError
+
+    def __repr__(self):
+        return 'F1, threshold: {}'.format(self.threshold)