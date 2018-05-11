fac0bbf327fc88c5320e336ea1d4bb13a472e786
yuyupopo <yuyupopo@snu.ac.kr>
2018-05-12 02:09:21 +0900

    update experiment manager

--- src/experiments.py
+++ src/experiments.py
@@ -8,6 +8,8 @@ import signal
 import argparse
 import fcntl
 from torch import cuda
+from threading import Lock
+import threading
 
 parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument("--mode", "-m", type=str, help="add, watch, end", default="add")
@@ -16,7 +18,11 @@ parser.add_argument("--config", "-c", type=str, help="path to config file")
 parser.add_argument("--log", "-l", type=str, help="optional stdout")
 
 JOBS = 'experiments'
-signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
+
+running = {}
+gpu_locks = [Lock() for c in cuda.device_count()] if cuda.is_available() else None
+# gpu_locks = [Lock(), Lock()]
 
 class Experiment():
     """a experiment object
@@ -29,40 +35,44 @@ class Experiment():
         self.log = log
 
         if gpu is None:
-            gpu = ','.join(range(cuda.device_count())) if cuda.is_available() else None
-        elif gpu != "None":
-            gpu = ','.join(gpu)
-                
+            gpu = [str(idx) for idx in range(cuda.device_count())] if cuda.is_available() else None
+
         self.gpu = gpu
-        
 
     @staticmethod
     def makeExperiment(e):
-        e = e.split('|')
-        return Experiment(*e)
+        gpu, config, log = e.split('|')
+        gpu = gpu.split(',')
+        return Experiment(gpu, config, log)
 
     def __str__(self):
-        return "{}|{}|{}\n".format(self.gpu, self.config, self.log)
+        gpu = ','.join(self.gpu)
+        return "{}|{}|{}\n".format(gpu, self.config, self.log)
         
 def run(experiment):
+    
     env = os.environ.copy()
-    env['CUDA_VISIBLE_DEVICES'] = experiment.gpu
+
+    if experiment.gpu is not None: # and cuda.is_available():
+        
+        gpu = experiment.gpu
+        gpu.sort()
+        for idx in gpu:
+            gpu_locks[int(idx)].acquire()
+
+        env['CUDA_VISIBLE_DEVICES'] = ' '.join(experiment.gpu)
+
     command = ['python3', 'driver.py', '--config', experiment.config]
 
     if experiment.log is None:
-        p = subprocess.Popen(command, env=env)
-        p.wait()
-    else:
-        p = subprocess.Popen(command, env=env, stdout=subprocess.PIPE)
+        p = subprocess.Popen(command, env=env, universal_newlines=True)
         slack_message('new experiment pid({})'.format(p.pid))
-        with open(experiment.log, 'wb') as log:
-            while True:
-                line = p.stdout.readline()
-                if line != b'':
-                    log.write(line)
-                else: 
-                    break
-            
+        running[p.pid] = experiment, None
+    else:
+        log = open(experiment.log, 'w')
+        p = subprocess.Popen(command, env=env, universal_newlines=True, stdout=log)
+        slack_message('new experiment pid({})'.format(p.pid))
+        running[p.pid] = experiment, log
 
 
 def watch():
@@ -79,17 +89,15 @@ def watch():
                     exit(0)
 
                 print("New experiment start: {}".format(experimentNum))
-                slack_message("New experiment start: {}".format(experimentNum)) #, "#driver-py")
                 experimentNum += 1
             
                 experiment = Experiment.makeExperiment(e)
-                run(experiment)
+                thread = threading.Thread(target=run, args=(experiment,))
+                thread.start()
                 
 
 def add(args):
     with open(JOBS, 'w', ) as fifo:
-        # fd = fifo.fileno()
-        # fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK)
         fifo.write(str(Experiment(args.gpu, args.config, args.log)))
         print("Added new experiment")
 
@@ -99,7 +107,30 @@ def end():
         fcntl.fcntl(fd, fcntl.F_SETFL, os.O_NONBLOCK)
         fifo.write('end')
 
+def done(signum, p):
+    
+    print('experiment done')
+
+    while True:
+        try:
+            pid, status = os.waitpid(-1, os.WNOHANG|os.WUNTRACED|os.WCONTINUED)
+            experiment, log = running[pid]
+
+            print('pid {} gpu {}----------'.format(pid, experiment.gpu))
+            
+            if log is not None:
+                log.close()
+
+            gpu = experiment.gpu
+            gpu.sort()
+            for idx in gpu:
+                gpu_locks[int(idx)].release()
+        except:
+            break
+
 def main(args):
+    signal.signal(signal.SIGCHLD, done)
+
     try:
         os.mkfifo(JOBS)
     except OSError as oe:
